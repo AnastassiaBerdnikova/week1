58a36168f40e2daf969a80f4d7fbd4bf72eef7b1
Anastassia Brd. <anastassia.berdnikova@gmail.com>
2024-03-04 15:20:12 +0200

    week 1 code

---
+++ server.js
@@ -0,0 +1,15 @@
+//console.log("Hello,world!");
+
+let firstName = 'Anastassia';
+let lastName = 'Berdnikova';
+
+//console.log(firstName);
+
+//console.log("Hello, "+ firstName + "!");
+
+//template literals
+
+//console.log(`Hello, ${firstName}!`)
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log (greeting);
